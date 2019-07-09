3b193aa397f43f0195b4d1f3524ab66dfc193b04
deng719 <543354409@qq.com>
2019-07-09 15:12:55 +0800

    。。。

---
+++ laiqio-sellergoods/laiqio-sellergoods-service/src/main/java/com/laiqio/sellergoods/SellerApplication.java
@@ -0,0 +1,16 @@
+package com.laiqio.sellergoods;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTransactionManagement
+public class SellerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SellerApplication.class,args);
+    }
+}
--- leyou-order/leyou-order-service/src/main/java/com/leyou/order/mapper/OrderMapper.java
+++ leyou-order/leyou-order-service/src/main/java/com/leyou/order/mapper/OrderMapper.java
@@ -2,7 +2,6 @@ package com.leyou.order.mapper;
 
 import com.leyou.order.pojo.Order;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
